97b6d7f4206cfad76b80b534bb7d371bf15b92e1
Loïc Marais <maraisloic1208@gmail.com>
2019-02-11 14:48:47 +0100

    1 à 5

---
+++ intro/ex04/script.js
@@ -0,0 +1,3 @@
+function alertbox() {
+    alert("Nom : " + document.getElementById("nom").value + "\n" + "Prénom : " + document.getElementById("prenom").value + "\n" + "Ville : " + document.getElementById("ville").value);
+}
---
+++ intro/ex05/script.js
@@ -0,0 +1,5 @@
+function calcul() {
+    alert(Math.trunc(document.getElementById("premier_nombre").value)*document.getElementById("deuxieme_nombre").value);
+}
+
+//Math.trunc
